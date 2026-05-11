92836576f5723417b4ad3d539cbadd8551e843a3
AAISkuy <ahmadfizalal21@gmail.com>
2026-05-11 14:48:31 +0700

    update progress Latihan 2 Flutter -blom kelar cuy

--- lib/latihan2Flutter.dart
+++ lib/latihan2Flutter.dart
@@ -6,7 +6,42 @@ class layoutKomp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        toolbarHeight: 100,
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+        title:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+        Text("CareSkin+",
+        style: TextStyle(color: Color(0xFFEECBAF), fontSize:24),),
+        Text("Aplikasi Rekomendasi Skincare Sesuai Jenis Kulit Anda",
+        style: TextStyle(color: Color(0xFFF0A8D6), fontSize:14),),
+        ],
+      ),
+    ),
+
+     body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 43, 216, 202),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(1, 1),
+                    blurStyle: BlurStyle.outer,
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+            )
+            ]
+        )
+    )
     );
   }
 }
