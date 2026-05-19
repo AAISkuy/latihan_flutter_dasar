77558312976f3c67d24473eeea1a212397c8ab1d
AAISkuy <ahmadfizalal21@gmail.com>
2026-05-19 14:49:28 +0700

    Update cicil tugas Login

--- lib/formLogin.dart
+++ lib/formLogin.dart
@@ -10,6 +10,63 @@ class formLogin extends StatefulWidget {
 class _formLoginState extends State<formLogin> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(25),
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFFF6F1EE), Color.fromARGB(255, 214, 236, 238)],
+          ),
+        ),
+
+        child: Center(
+          child: Container(
+            width: 320,
+            height: 650,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(35),
+            ),
+
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 70,
+                  width: 70,
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/images/kp2_dm.gif"),
+                    ),
+                  ),
+                ),
+
+                Text(
+                  "Skinoura",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF7C9A92),
+                  ),
+                ),
+
+                Text(
+                  "Your skincare journey begins here.",
+                  style: TextStyle(fontSize: 14, color: Color(0xFF7C9A92)),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
