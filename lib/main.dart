3d327a8dcb66d928025aef42efabaa96d9b8adfc
AAISkuy <ahmadfizalal21@gmail.com>
2026-05-20 08:16:32 +0700

    update hapus file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:latihan_flutter_dasar/formLanding.dart';
+import 'package:latihan_flutter_dasar/formLogin.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: formLanding(),
+      home: formLogin(),
     );
   }
 }
